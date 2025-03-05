60dac4ff7c5c5c6e2875fc0bff69e5dc4b438d35
Mert-Gocmen <mert33gocmen@gmail.com>
2025-03-05 03:49:43 +0300

    Öğrenci Clası tamamlandı.

--- src/Gun01/Ogrenci.java
+++ src/Gun01/Ogrenci.java
@@ -5,4 +5,42 @@ public class Ogrenci {
     private String ad;
     private String soyad;
 
+    public Ogrenci(int okulNo, String soyad, String ad) {
+        this.okulNo = okulNo;
+        this.soyad = soyad;
+        this.ad = ad;
+    }
+
+    public int getOkulNo() {
+        return okulNo;
+    }
+
+    public void setOkulNo(int okulNo) {
+        this.okulNo = okulNo;
+    }
+
+    public String getAd() {
+        return ad;
+    }
+
+    public void setAd(String ad) {
+        this.ad = ad;
+    }
+
+    public String getSoyad() {
+        return soyad;
+    }
+
+    public void setSoyad(String soyad) {
+        this.soyad = soyad;
+    }
+
+    @Override
+    public String toString() {
+        return "Ogrenci{" +
+                "okulNo=" + okulNo +
+                ", ad='" + ad + '\'' +
+                ", soyad='" + soyad + '\'' +
+                '}';
+    }
 }
