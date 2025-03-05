4741007c9af8889d7774a176a926f3b36782f28b
Mert-Gocmen <mert33gocmen@gmail.com>
2025-03-05 03:54:47 +0300

    Okul Main eklendi.

---
+++ src/Gun01/Okul.java
@@ -0,0 +1,9 @@
+package Gun01;
+
+public class Okul {
+    public static void main(String[] args) {
+
+        Ogrenci ogr1=new Ogrenci("Mert","Göçmen",1);
+        System.out.println(ogr1);
+    }
+}
